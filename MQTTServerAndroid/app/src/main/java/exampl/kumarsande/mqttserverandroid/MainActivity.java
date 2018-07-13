43767cddfa27ec4719040278e45cef7c89cad81a
sandeep Kumar <sandeep.g1983@gmail.com>
2018-07-13 11:26:06 +0530

    MQTT broker for Android

---
+++ MQTTServerAndroid/app/src/main/java/exampl/kumarsande/mqttserverandroid/MainActivity.java
@@ -0,0 +1,44 @@
+package exampl.kumarsande.mqttserverandroid;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+import org.eclipse.moquette.server.Server;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        try {
+            new Server().startServer();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+   /* @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }*/
+
+    /*@Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
+}
